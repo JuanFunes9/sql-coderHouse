503cf6ddfc6ebef70709e310a41a99a48596aece
Juan Funes <juan.funes.96@outlook.com>
2022-10-03 19:53:31 -0300

    reentrega de funciones

--- sql/Formato + Funes.sql
+++ sql/Formato + Funes.sql
@@ -1,12 +1,12 @@
 USE hospital;
 DELIMITER $$
-CREATE FUNCTION get_edad_by_fecha_nac( p_id INT, p_fecha_nac date )
+CREATE FUNCTION get_edad_paciente_by_fecha_nac( p_id INT, p_fecha_nac date )
 RETURNS int
-DETERMINISTIC
+READS SQL DATA
 BEGIN
 
 	DECLARE edad INT;
-    SELECT YEAR(curdate())-YEAR(p_fecha_nac)
+    SELECT floor(datediff(curdate(),p_fecha_nac) / 365)
 	INTO edad
     FROM pacientes
     WHERE pacientes.id_paciente = p_id;
@@ -16,7 +16,7 @@ END$$
 
 CREATE FUNCTION get_cantidad_consultas( d_id INT )
 RETURNS int
-DETERMINISTIC
+READS SQL DATA
 BEGIN
 
 	DECLARE cant_consultas INT;
